95bd4f4124bbc44322497c55bf306cdc1fc5abc9
woduq1414 <jjy37777@naver.com>
2020-12-18 22:46:47 +0900

    Update config.py

--- config.py
+++ config.py
@@ -55,6 +55,16 @@ else:
 
     REDIS_URL = os.environ.get('REDIS_URL', None)
     FERNET_KEY = os.environ.get('FERNET_KEY', None)
+
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[0])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[1])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[0])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[1])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[0])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[1])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[0])
+    print(os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[1])
+    
     SESSION_REDIS = redis.Redis(host=os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[0],
                                 port=os.environ.get('REDIS_URL', None).split("redis://:")[1].split(":")[1])
 
